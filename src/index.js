d7d3f4ef2cdfc1478a509086eb02a56335800fec
Akash102938 <akashjordan399@gmail.com>
2025-10-06 11:55:28 +0530

    commit changes

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+//require('dotenv').config({path: './env'})
+import connectDB from "./db/index.js"
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: './.env'
+})
+
+
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`⚙️ Server is running at port : ${process.env.PORT} `);
+        
+    })
+})
+.catch((error)=>{
+    console.log("MONGOBD CONNCECTION FAILED !!",error);
+    
+})
+
+
+
+
+// import express from "express"
+
+// const app = express()
+
+// (async ()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         app.on("error", ()=>{
+//             console.log("ERROR: ",error);
+//             throw error
+//         })
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`);
+            
+//         })
+//     } catch (error) {
+//         console.log("ERROR: ",error);
+//         throw error
+//     }
+// })()
