6869473dd424849d8d2706bc52202d4fce734492
Akash Pandey <akashjordan399@gmail.com>
2025-10-11 15:15:12 +0530

    commit changes

--- src/app.js
+++ src/app.js
@@ -13,4 +13,9 @@ app.use(express.urlencoded({extended: true,limit:"20kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes import
+import userRouter from './routes/user.routes.js'
+
+app.use("/users",userRouter)
+
 export {app}
